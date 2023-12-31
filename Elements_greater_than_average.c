50932c16c77025f1ff7f6a96e51b9d1c5e58168d
chandrikanallamilli <147594864+chandrikanallamilli@users.noreply.github.com>
2023-12-31 14:49:15 +0530

    #include<stdio.h> int main() {     int a,sum=0,i;     scanf("%d",&a);     int ar[a];     for(i=0;i<a;i++)     {         scanf("%d",&ar[i]);         sum+=ar[i];     }     int avg=sum/a,c=0;     for(i=0;i<a;i++)     {         if(avg<=ar[i])         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum=0,i;
+    scanf("%d",&a);
+    int ar[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&ar[i]);
+        sum+=ar[i];
+    }
+    int avg=sum/a,c=0;
+    for(i=0;i<a;i++)
+    {
+        if(avg<=ar[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
