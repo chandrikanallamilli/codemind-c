586b7e1c17d639327c36ada342e32f5f423544b7
chandrikanallamilli <147594864+chandrikanallamilli@users.noreply.github.com>
2023-12-31 14:43:24 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     int ar[a];     for(i=0;i<a;i++)     {         scanf("%d",&ar[i]);         if(ar[i]%2==1)         {             printf("%d ",ar[i]);         }     }     for(i=0;i<a;i++)     {         if(ar[i]%2==0)         {             printf("%d ",ar[i]);         }     } }

---
+++ Array_of_odd_and_even_elements.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    int ar[a];
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&ar[i]);
+        if(ar[i]%2==1)
+        {
+            printf("%d ",ar[i]);
+        }
+    }
+    for(i=0;i<a;i++)
+    {
+        if(ar[i]%2==0)
+        {
+            printf("%d ",ar[i]);
+        }
+    }
+}
