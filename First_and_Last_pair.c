0891983b923f6817fd8fe75c5dbfe9e08a1a62a6
chandrikanallamilli <147594864+chandrikanallamilli@users.noreply.github.com>
2023-12-31 16:06:31 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int k=n%2;     for(i=0;i<n/2;i++)     {         printf("%d %d ",arr[i],arr[n-i-1]);     }     if(k)     printf("%d 0",arr[n/2]); }

---
+++ First_and_Last_pair.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int k=n%2;
+    for(i=0;i<n/2;i++)
+    {
+        printf("%d %d ",arr[i],arr[n-i-1]);
+    }
+    if(k)
+    printf("%d 0",arr[n/2]);
+}
