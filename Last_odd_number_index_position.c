c7f33e05e9d5407d1e29989552936fac80da2cd7
chandrikanallamilli <147594864+chandrikanallamilli@users.noreply.github.com>
2023-12-31 15:25:43 +0530

    #include<stdio.h> int main() {     int a,i;     scanf("%d",&a);     int ar[a],s=0;     for(i=0;i<a;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<a;i++)     {         if(ar[i]%2==1)         {             s=i;         }     }     printf("%d",s); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    int ar[a],s=0;
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<a;i++)
+    {
+        if(ar[i]%2==1)
+        {
+            s=i;
+        }
+    }
+    printf("%d",s);
+}
